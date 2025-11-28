fb7bb5d875d138f933b10e97627d16311073e328
Zaki <zakifadhilarahman@mail.ugm.ac.id>
2025-11-28 21:45:57 +0700

    UI Slicing for TitlePage, LoginPage, and RegisterPage

--- IkanLogger2/Views/TitlePage.xaml.cs
+++ IkanLogger2/Views/TitlePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using IkanLogger2.Views;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,22 +13,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace IkanLogger2
+namespace IkanLogger2.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TitlePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TitlePage : Page
     {
-        public MainWindow()
+        public TitlePage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new TitlePage());
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Hide();
+            NavigationService.Navigate(new LoginPage());
         }
     }
 }
